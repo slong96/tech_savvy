b78238d2bb2f84dc4963a603051460b7f3a691d2
slong96 <94485068+slong96@users.noreply.github.com>
2023-06-19 17:13:25 -0500

    host

--- tech_savvy_blog/settings.py
+++ tech_savvy_blog/settings.py
@@ -27,14 +27,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = False
 
-ALLOWED_HOSTS = []
-
-DEBUG = 'RENDER' not in os.environ
-
-RENDER_EXTERNAL_HOSTNAME = os.getenv('RENDER_EXTERNAL_HOSTNAME')
-if RENDER_EXTERNAL_HOSTNAME:
-    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
